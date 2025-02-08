4c11c38f28df070163f63b2ac31f54e057a8adc9
Daniel C <Danielcontaf1996@gmail.com>
2025-02-08 20:14:45 +0100

    fix

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { LogoComponent } from '../logo/logo.component';
-import { MainButtonComponent } from '../main-button/main-button.component';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { bootstrapLinkedin, bootstrapGithub } from '@ng-icons/bootstrap-icons'
 import { RouterLink, RouterLinkActive } from '@angular/router';
@@ -8,7 +7,7 @@ import { TranslocoDirective } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-footer',
-  imports: [LogoComponent, MainButtonComponent, NgIcon, RouterLink, TranslocoDirective, RouterLinkActive],
+  imports: [LogoComponent, NgIcon, RouterLink, TranslocoDirective, RouterLinkActive],
   viewProviders: [provideIcons({ bootstrapLinkedin, bootstrapGithub })],
   templateUrl: './footer.component.html',
 })
