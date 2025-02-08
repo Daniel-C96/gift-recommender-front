c1f6a9cd06e7c05ee83c19f660f870f7a041f3ed
Daniel C <Danielcontaf1996@gmail.com>
2025-02-08 20:30:27 +0100

    changes in environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-        //API_URL: "http://localhost:8080/api/v1/"
-        API_URL: "giftrecommender-production.up.railway.app/api/v1/"
+        production: true,
+        API_URL: "https://giftrecommender-production.up.railway.app/api/v1/"
 };
