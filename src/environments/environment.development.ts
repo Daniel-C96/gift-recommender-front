03af0b7112f9aef7fe0d35ca56669ee1e60c0a9a
Daniel C <Danielcontaf1996@gmail.com>
2025-02-08 10:52:42 +0100

    changes

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+        //API_URL: "http://localhost:8080/api/v1/"
+        API_URL: "https://giftrecommender-production.up.railway.app/api/v1/"
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+        //API_URL: "http://localhost:8080/api/v1/"
+        API_URL: "giftrecommender-production.up.railway.app/api/v1/"
+};
