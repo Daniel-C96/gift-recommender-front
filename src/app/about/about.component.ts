36a14f767878be3face15377fb8b202a49964635
Daniel C <Danielcontaf1996@gmail.com>
2025-02-10 04:53:02 +0100

    about and contact changed layout

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TranslocoDirective } from '@jsverse/transloco';
 import { MainButtonComponent } from "../components/main-button/main-button.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-about',
-  imports: [TranslocoDirective, MainButtonComponent],
+  imports: [TranslocoDirective, MainButtonComponent, RouterLink],
   templateUrl: './about.component.html',
 })
 export class AboutComponent {
